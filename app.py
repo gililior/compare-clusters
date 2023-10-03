e9b0d3a12c68b169d682ab5c4de4620364a39720
gililior <gili.lior96@gmail.com>
2023-10-04 00:13:00 +0300

    change to subheader

--- app.py
+++ app.py
@@ -24,15 +24,15 @@ def main():
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        st.header("By document coverage")
+        st.subheader("By document coverage")
         st.write(clusters_by_doc_coverage)
 
     with col2:
-        st.header("By corpus coverage")
+        st.subheader("By corpus coverage")
         st.write(clusters_by_corpus_coverage)
 
     with col3:
-        st.header("Combined coverage")
+        st.subheader("Combined coverage")
         st.write(clusters_by_combined_coverage)
 
 
